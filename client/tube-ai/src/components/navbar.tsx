3a3d4b475422e5367257b806db0c321f3c6ad42a
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-11-14 17:16:19 +0530

    feat: added navbar

---
+++ client/tube-ai/src/components/navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-transparent bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="flex items-center">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-mono">Tube.ai</span>
+                </div>
+                <div className="space-x-4">
+                    {/* <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Generate Quote</button> */}
+                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm relative top-1 text-gray-500 rounded-lg hover:bg-opacity-50 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}
