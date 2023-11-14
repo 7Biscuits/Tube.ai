ad709ef7c6902c151a627f6708e32a8aaf926bc5
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-11-14 14:04:48 +0530

    moved files to api folder

--- src/helpers/getVideoId.ts
+++
@@ -1,9 +0,0 @@
-export const getVideoId = (yturl: string): string => {
-  var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-  var match = yturl.match(regExp);
-  if (match && match[2].length == 11) {
-    return match[2];
-  } else {
-    return "error extracting video id";
-  }
-};
--- src/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getSummary } from "./controllers/getSummary";
-import { askQuestion } from "./controllers/askQuestion";
-
-export const router = Router();
-router.route("/summarize").get(getSummary);
-router.route("/question").get(askQuestion);
