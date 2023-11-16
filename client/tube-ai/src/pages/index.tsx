218241027777dbc84120b24b8d4dd595e2f5a167
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-11-16 15:43:51 +0530

    feat: get summary from api

--- client/tube-ai/src/pages/index.tsx
+++ client/tube-ai/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import { getSummary } from "@/helpers/getSummary";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleGetSummary = async (yturl: string): Promise<void> => {
+    const res = await getSummary(yturl);
+    console.log(res);
+  };
   return (
     <>
       <Navbar />
@@ -13,7 +18,8 @@ export default function Home() {
       >
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
           <div className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-[180] h-[37]" />
-          <Hero />
+          {/* <Hero handleClick={(yturl: string) => { getSummary(yturl) }} /> */}
+          <Hero onGetSummary={(yturl: string) => handleGetSummary(yturl)} />
         </div>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
